d5f0c82708509d1155abd53578df85dc2a49d6e5
Enrique Marrufo <ChachoJones@users.noreply.github.com>
2016-07-21 19:46:58 -0400

    IROP Main Method
    Most recent IROP Main Method with pizza wings and bread tests.

--- IROP.java
+++ IROP.java
@@ -12,14 +12,27 @@ import irop.Bread.BreadType;
 public class IROP {
 
     public static void main(String[] args) {
-        Bread myBread = new Bread(BreadType.BreadSticks) {};
         
-        // Getting all variables values
+        Pizza myPizza = new Pizza("LARGE", "HAND_TOSSED");
+        System.out.println("$" + myPizza.getPrice());
+        System.out.println(myPizza.getDescription());
+        System.out.println(myPizza.toString());
+        
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+        
+        Wings myWings = new Wings("boneless", "hot", 20);
+        System.out.println(myWings.getQty());
+        System.out.println(myWings.getType());
+        System.out.println("$" + myWings.getPrice());
+        System.out.println(myWings.getDescription());
+        System.out.println(myWings.toString());
+        
+        System.out.println("+++++++++++++++++++++++++++++++++++++");
+        
+        Bread myBread = new Bread(BreadType.BreadSticks) {};
         System.out.println(myBread.getName());
         System.out.println("$" + myBread.getPrice());
         System.out.println(myBread.getDescription());
-        
-        // Displaying toString
         System.out.println(myBread.toString());
 
     }// Main
